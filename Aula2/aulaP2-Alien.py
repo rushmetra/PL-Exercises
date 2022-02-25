6d58c2e0b69f75778a9d0c2db51f1ce83adf7d5a
rushmetra <a93179@alunos.uminho.pt>
2022-02-25 17:15:37 +0000

    Matriculas feito e inicio latlon

--- Aula2/aulaP2-Alien.py
+++ Aula2/aulaP2-Alien.py
@@ -1,6 +1,16 @@
 import re
 import sys 
 
+"""
+Enunciado:
+Tem que começar com '_' ou '.'
+Seguido de um numero
+Mais qq coisa sobre as letras
+Tem que acabar com letras ou com underscore '-'
+
+"""
+
+
 # Expressão regular para o Alien Username do problema1: 
 p = re.compile(r'^(_|\.)\d+[a-zA-Z]{3,}_?$')
 
