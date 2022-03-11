835746d45edfa95ccee0f28e6110e3cba27a4bf8
rushmetra <a93179@alunos.uminho.pt>
2022-03-11 16:30:37 +0000

    Pessoas.py feito sozinho, ta top

--- Aula4/pessoas.py
+++ Aula4/pessoas.py
@@ -5,8 +5,11 @@ Monteiro, G.
 Ronaldo, C.
 '''
 
+# Fiz esta expressão -> ([A-Z])\w*[\s]+([A-Z]\w*) e capturou (C)ristiano (Ronaldo)
 import re, sys
 
 for linha in sys.stdin:
-    linha = re.sub(r'', r'', linha)
+    linha = re.sub(r'([A-Z])\w*[\s]+([A-Z]\w*)', r'\2, \1.', linha)
     print(linha, end="")
+    
+    
