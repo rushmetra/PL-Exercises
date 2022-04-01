9ee92a78a8ecdd995cbab673dcab565067c81b83
rushmetra <a93179@alunos.uminho.pt>
2022-04-01 17:44:38 +0100

    End of class 7

--- Aula7/calc_sin.py
+++ Aula7/calc_sin.py
@@ -17,8 +17,54 @@ def p_Comando_Escrita(p):
 
 def p_Comando_Leitura(p):
     "Comando : '?' id"
-    
+    valor = input("Introduza um valor inteiro: ")
 
+def p_Comando_dump(p):
+    "Comando : DUMP"
+    print(p.parser.registos)
+
+def p_Exp_ad(p):
+    "Exp : Exp '+' Termo"
+    p[0] = p[1] + p[3]
+
+def p_Exp_sub(p):
+    "Exp : Exp '-' Termo"
+    p[0] = p[1] - p[3]
+
+def p_Exp_termo(p):
+    "Exp : Termo"
+    p[0] = p[1]
+
+def p_Termo_mul(p):
+    "Termo : Termo '*' Fator"
+    p[0] = p[1] * p[3]
+
+def p_Termo_div(p):
+    "Termo : Termo '/' Fator"
+    if p[3] != 0:
+        p[0] = p[1] / p[3]
+    else: 
+        print("ERRO: Divisão por 0...")
+
+def p_Termo_fator(p):
+    "Termo : Fator"
+    p[0] = p[1]
+
+def p_Fator_grupo(p):
+    "Fator : '(' Exp ')'"
+    p[0] = p[2]
+
+def p_Fator_num(p):
+    "Fator : num"
+    p[0] = p[1]
+
+def p_Fator_id(p):
+    "Fator : id"
+    if p[1] in p.parser.registos.keys():
+        p[0] = p.parser.registos[p[1]]
+    else:
+        print("ERRO: Registo inexistente -", p[1])
+        p[0] = 0 # Para o programa não estourar qnd der erro!
 
 def p_error(p):
     print('Erro sintático: ', p)
@@ -35,7 +81,3 @@ import sys
 for linha in sys.stdin:
     parser.success = True
     parser.parse(linha)
-    if parser.success:
-        print("Frase válida: ", linha)
-    else:
-        print("Frase inválida... Corrija e tente novamente!")
